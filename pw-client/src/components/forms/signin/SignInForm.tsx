fa71b44405fc128576439fde36e14489fe5b8a92
Shakhboz Khalikov <cool_man1111@mail.ru>
2020-12-07 23:01:54 +0500

    [+]Added validation for signin

--- pw-client/src/components/forms/signin/SignInForm.tsx
+++ pw-client/src/components/forms/signin/SignInForm.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Field, reduxForm, SubmitHandler } from "redux-form";
+import { Field, FormErrors, reduxForm, SubmitHandler } from "redux-form";
+import {
+  REQUIRED,
+  TOO_LONG,
+  TOO_SHORT,
+} from "../../../redux/models/ValidationErrors";
+import { renderField } from "../../input/renderInput";
 
 import s from "./SignInForm.module.scss";
 
@@ -7,22 +13,41 @@ interface SignInFormProps {
   handleSubmit: SubmitHandler;
 }
 
+interface SignInFormData {
+  email?: string;
+  password?: string;
+}
+
+const validate = (values: SignInFormData) => {
+  let errors: FormErrors<SignInFormData> = {};
+
+  if (!values.email) {
+    errors.email = REQUIRED;
+  }
+
+  if (!values.password) {
+    errors.password = REQUIRED;
+  }
+
+  return errors;
+};
+
 const SignInForm = ({ handleSubmit }: SignInFormProps) => {
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <Field
-          component={"input"}
+          component={renderField}
           name={"email"}
-          placeholder={"Email"}
+          label={"Email"}
           className={s.form_input}
         />
       </div>
       <div>
         <Field
-          component={"input"}
+          component={renderField}
           name={"password"}
-          placeholder={"Password"}
+          label={"Password"}
           type={"password"}
           className={s.form_input}
         />
@@ -34,4 +59,4 @@ const SignInForm = ({ handleSubmit }: SignInFormProps) => {
   );
 };
 
-export default reduxForm({ form: "signin" })(SignInForm);
+export default reduxForm({ form: "signin", validate })(SignInForm);
